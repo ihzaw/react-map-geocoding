92c24dca7c3c02eb6ee27c40fd826c7014badf93
ihzaw <ihzanantama@gmail.com>
2022-02-13 02:46:21 +0800

    add language switcher

--- geocoding-demo/src/components/NavigationBar.js
+++ geocoding-demo/src/components/NavigationBar.js
@@ -1,5 +1,6 @@
 import { ToggleDarkModeBtn } from "./ToggleDarkModeBtn";
 import { motion } from "framer-motion";
+import { LanguageSwitcher } from "./LanguageSwitcher";
 
 export const Nav = ({ darkMode, setDarkMode }) => {
   return (
@@ -11,8 +12,9 @@ export const Nav = ({ darkMode, setDarkMode }) => {
         animate={{ backgroundColor: "#ffffff", transition: {duration: 0.5} }}
         exit={{ backgroundColor: "#0f172a" }}
       >
-        <div className="col-span-2 flex justify-start items-center px-4">
+        <div className="mb-4 grid grid-cols-2 col-span-2 items-center px-4 font-bold text-slate-900">
           <ToggleDarkModeBtn setDarkMode={setDarkMode} darkMode={darkMode} />
+          <LanguageSwitcher />
         </div>
       </motion.div>}
       {darkMode && <motion.div
@@ -22,8 +24,9 @@ export const Nav = ({ darkMode, setDarkMode }) => {
         animate={{ backgroundColor: "#0f172a", transition: {duration: 0.5} }}
         exit={{ backgroundColor: "#ffffff" }}
       >
-        <div className="col-span-2 flex justify-start items-center px-4">
+        <div className="grid grid-cols-2 col-span-2 items-center px-4 font-bold text-[#19817c]">
           <ToggleDarkModeBtn setDarkMode={setDarkMode} darkMode={darkMode} />
+          <LanguageSwitcher />
         </div>
       </motion.div>}
     </>
--- geocoding-demo/src/views/Homepage.js
+++ geocoding-demo/src/views/Homepage.js
@@ -4,7 +4,9 @@ export const Home = ({ darkMode, setDarkMode }) => {
     return (
         <div>
             <Nav darkMode={darkMode} setDarkMode={setDarkMode}/>
-            <div>{darkMode ? "dark" : "light"}</div>
+            <div className="">
+
+            </div>
         </div>
     )
 }
